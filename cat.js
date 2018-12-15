23384e802248a8af222218150be12682cb0904e6
etc <trashbxg@gmail.com>
2018-12-15 14:24:56 +0100

    Update cat.js

--- cat.js
+++ cat.js
@@ -142,6 +142,7 @@ if(cmd === 'util') {
   .setFooter(`${client.user.username} | By: etcroot#6666`)
   .setDescription([`
       \`${cfg.prefix}remineme\` → forget much?
+      \`${cfg.prefix}gaycalc\` → calculate gayness.
   `])
   return message.channel.send(embed);
 }
@@ -383,7 +384,28 @@ if(cmd === 'util') {
             console.log(err);
             return message.channel.send(`No results found for **${search}**!`)
         })
-	}
+  }
+  if(command === "gaycalc") {
+    const gay = [
+      "You're pretty straight.",
+      "Eeeeh, you're leaning straight.",
+      "Bicurious.",
+      "You're bi, you just don't know it yet.",
+      "Definitely bisexual.",
+      "Leaning a bit more to men.",
+      "Halfway out of your closet.",
+      "You're almost as gay as me!",
+      "You've just told your parents how gay you are, i'm proud of you!",
+      "You're a fully fledged homosexual being."
+    ];
+    let member = message.mentions.members.first();
+    var randomgay = gay[Math.floor(Math.random()*gay.length)];
+    if(!member) {
+    return message.reply(`${randomgay}`);
+    } else {
+      return message.channel.send(`${member}, ${randomgay}`)
+    }
+  }
 });
 
 client.login(cfg.token);
