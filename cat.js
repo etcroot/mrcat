6ca310672441c0bbb6a6917d7350edff99c84bec
etc <trashbxg@gmail.com>
2018-12-15 14:32:47 +0100

    Update cat.js

--- cat.js
+++ cat.js
@@ -143,6 +143,7 @@ if(cmd === 'util') {
   .setDescription([`
       \`${cfg.prefix}remineme\` → forget much?
       \`${cfg.prefix}gaycalc\` → calculate gayness.
+      \`${cfg.prefix}weebcalc\` → calculate weebness.
   `])
   return message.channel.send(embed);
 }
@@ -405,6 +406,28 @@ if(cmd === 'util') {
     return message.reply(`${randomgay}`);
     } else {
       return message.channel.send(`${member}, ${randomgay}`)
+    }
+  }
+  // Weeb calculator
+  if(command === "weebcalc") {
+    const weeb = [
+      "You're a full on weaboo, you own katanas & bodypillows... Still in your moms basement huh?",
+      "You've watched like 1k+ animes & read the same amount of manga as you've watched anime.",
+      "You're pretty much a cosplayer at this point, not in a good way.",
+      "You're obsessed with anime, you just don't watch it all the time.",
+      "You're a normie/weeb mixbreed.",
+      "You just started watching anime.",
+      "You don't watch anime but you like the art.",
+      "You think anime is annoying.",
+      "You hate anime with a passion.",
+      "BURN THE WEEBS!"
+    ];
+    let member = message.mentions.members.first();
+    var randomweeb = weeb[Math.floor(Math.random()*weeb.length)];
+    if(!member) {
+    return message.reply(`${randomweeb}`);
+    } else {
+      return message.channel.send(`${member}, ${randomweeb}`)
     }
   }
 });
